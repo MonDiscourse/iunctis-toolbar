c51defed4006b12387e84b7796da736d0552bcdf
iunctis <steven.abgrall@gmail.com>
2017-02-23 00:55:48 +0100

    Version 2.1
    Nettoyage du plugin et suppression du CSS perso

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,9 @@
 # name: iunctis-toolbar
 # about: Ajout de boutons sur l'éditeur
-# version: 2.0
-# authors: Matthew Wilkin et Steven
+# version: 2.1
+# authors: Steven
 # url: https://github.com/iunctis/iunctis-toolbar
 
 enabled_site_setting :iunctistlb_ui_enabled
 
-register_asset "javascripts/iunctis_bbcode.js", :server_side
-
-register_asset 'stylesheets/iunctisbonus.scss'
-register_asset 'stylesheets/iunctisemojis.scss'
 register_asset 'stylesheets/iunctistlb.scss'
