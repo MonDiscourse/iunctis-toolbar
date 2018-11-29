cd8e5bdb45a5045dd7f82bc3bcfd88ee9c5ccd23
iunctis <steven.abgrall@gmail.com>
2018-11-29 03:02:45 +0100

    FIX

--- plugin.rb
+++ plugin.rb
@@ -9,7 +9,6 @@ enabled_site_setting :iunctistlb_ui_enabled
 register_asset 'stylesheets/iunctistlb.scss'
 
 register_svg_icon "fa-underline" if respond_to?(:register_svg_icon)
-register_svg_icon "far-newspaper" if respond_to?(:register_svg_icon)
 register_svg_icon "fa-indent" if respond_to?(:register_svg_icon)
 register_svg_icon "fa-align-center" if respond_to?(:register_svg_icon)
 register_svg_icon "fa-align-right" if respond_to?(:register_svg_icon)
