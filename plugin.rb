ee85efcf1798a20a04b07e8f8caa135b83afc3ef
mondiscourse <mondiscourse@gmail.com>
2024-12-22 09:57:53 +0100

    FA6

--- plugin.rb
+++ plugin.rb
@@ -9,10 +9,10 @@ enabled_site_setting :iunctistlb_ui_enabled
 
 register_asset 'stylesheets/iunctistlb.scss'
 
-register_svg_icon "fa-underline" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-indent" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-align-center" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-align-right" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-align-justify" if respond_to?(:register_svg_icon)
+register_svg_icon "underline" if respond_to?(:register_svg_icon)
+register_svg_icon "indent" if respond_to?(:register_svg_icon)
+register_svg_icon "align-center" if respond_to?(:register_svg_icon)
+register_svg_icon "align-right" if respond_to?(:register_svg_icon)
+register_svg_icon "align-justify" if respond_to?(:register_svg_icon)
 register_svg_icon "far-image" if respond_to?(:register_svg_icon)
 register_svg_icon "far-newspaper" if respond_to?(:register_svg_icon)
